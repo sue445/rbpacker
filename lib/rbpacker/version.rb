bfb9d0d014b5f5136ef3b04949a3d088f671279b
sue445 <sue445@sue445.net>
2026-09-06 16:18:43 +0900

    bundle gem --mit rbpacker

---
+++ lib/rbpacker/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Rbpacker
+  VERSION = "0.1.0"
+end
---
+++ spec/rbpacker_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Rbpacker do
+  it "has a version number" do
+    expect(Rbpacker::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
