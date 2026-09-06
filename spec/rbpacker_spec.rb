5d11e764acc361f2fcdc16ce137e7c785f409882
sue445 <sue445@sue445.net>
2026-09-06 16:38:00 +0900

    Fix failed spec

--- spec/rbpacker_spec.rb
+++ spec/rbpacker_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Rbpacker do
   it "has a version number" do
     expect(Rbpacker::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
