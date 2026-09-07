54d8283d2f7454b8dd6f5483971a9eafbb7cc66b
sue445 <sue445@sue445.net>
2026-09-07 23:28:12 +0900

    Enabled aggregate_failures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,10 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.define_derived_metadata do |meta|
+    meta[:aggregate_failures] = true
+  end
 end
 
 def spec_dir
